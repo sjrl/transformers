06cfce193accf74aff6d46f06457bbc7e7c721c9
Sebastian Husch Lee <sjrl423@gmail.com>
2023-05-19 22:43:47 +0200

    Updating targe modules

--- examples/pytorch/question-answering/run_qa.py
+++ examples/pytorch/question-answering/run_qa.py
@@ -429,27 +429,25 @@ def main():
         logger.info("Using PEFT for LoRA training.")
         from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
 
-        # TODO Can leave target_modules_mapping as None for now. There is an auto detection in peft.
-        # target_modules_mapping = {
-        #     "deberta": ["query_proj", "value_proj", "pos_query_proj"],
-        #     "bert": ["query", "value"],
-        #     "t5": ["q", "v"],
-        # }
-        # target_modules = None
-        # for key in target_modules_mapping:
-        #     if key in model_args.model_name_or_path:
-        #         target_modules = target_modules_mapping[key]
-        #         break
-        # if target_modules is None:
-        #     raise ValueError("Could not determine the target_modules to use in LoRA")
+        # Updated targe modules following recommendation from AdaLora paper
+        target_modules_mapping = {
+            "deberta-v2": ["query_proj", "key_proj", "value_proj", "dense"],
+            "bert": ["query", "value", "key", "dense"],
+            "t5": ["q", "k", "v", "o", "wi", "wo"],
+        }
+        target_modules = None
+        for key in target_modules_mapping:
+            if key in model_args.model_name_or_path:
+                target_modules = target_modules_mapping[key]
+                break
+        if target_modules is None:
+            raise ValueError("Could not determine the target_modules to use in LoRA")
 
         # Define LoRA Config
         lora_config = LoraConfig(
             r=16,
             lora_alpha=32,
-            # These target modules need to be updated based on model architecture.
-            # E.g. self.q and self.v exist for T5 but for Bert the equivalent would be self.query and self.value.
-            target_modules=None,
+            target_modules=target_modules,
             lora_dropout=0.05,
             bias="none",
             task_type=TaskType.QUESTION_ANS
