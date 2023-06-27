9d1565d2502576064103082659d69ab45dfb8f3d
Sebastian Husch Lee <sjrl423@gmail.com>
2023-06-27 07:44:40 +0200

    Updates to run_eval

--- examples/pytorch/question-answering/run_eval.py
+++ examples/pytorch/question-answering/run_eval.py
@@ -10,33 +10,35 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     datasets = {
-        "squad_v2": None,
-        "squad": None,
+        "squad_v2": [None],
+        "squad": [None],
         "squadshifts": ["amazon", "new_wiki", "nyt", "reddit"],
         "adversarial_qa": ["adversarialQA"],
         "squad_adversarial": ["AddOneSent"],
     }
 
-    for d, c in datasets.items():
-        if c is not None:
-            output_dir = os.path.join(args.model_path, f"eval_{d}_{c}")
-        else:
-            output_dir = os.path.join(args.model_path, f"eval_{d}")
-        raw_args = [
-            "--model_name_or_path", args.model_path,
-            "--dataset_name", d,
-            "--dataset_config_name", c,
-            "--output_dir", output_dir,
-            "--version_2_with_negative", "True",
-            "--max_seq_length", "512",
-            "--doc_stride", "128",
-            "--do_eval",
-            "--per_device_eval_batch_size", args.batch_size,
-            "--bf16_full_eval",
-            "--tf32", "True",
-            "--dataloader_num_workers", "6",
-            "--preprocessing_num_workers", "6",
-            "--eval_accumulation_steps", "2",
-            "--overwrite_output_dir", "False",
-        ]
-        main(raw_args=raw_args)
+    for dataset, configs in datasets.items():
+        for config in configs:
+            if config is not None:
+                output_dir = os.path.join(args.model_path, f"eval_{dataset}_{config}")
+                extra_args = ["--dataset_config_name", config]
+            else:
+                output_dir = os.path.join(args.model_path, f"eval_{dataset}")
+                extra_args = []
+            raw_args = [
+                "--model_name_or_path", args.model_path,
+                "--dataset_name", dataset,
+                "--output_dir", output_dir,
+                "--version_2_with_negative", "True",
+                "--max_seq_length", "512",
+                "--doc_stride", "128",
+                "--do_eval",
+                "--per_device_eval_batch_size", str(args.batch_size),
+                "--bf16_full_eval",
+                "--tf32", "True",
+                "--dataloader_num_workers", "6",
+                "--preprocessing_num_workers", "6",
+                "--eval_accumulation_steps", "2",
+                "--overwrite_output_dir", "False",
+            ] + extra_args
+            main(raw_args=raw_args)
