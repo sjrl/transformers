01787cb86da6f5fb1937a55471c36ef3230d34f0
Sebastian Husch Lee <sjrl423@gmail.com>
2023-09-22 09:30:08 +0200

    Update

--- examples/pytorch/question-answering/run_eval.py
+++ examples/pytorch/question-answering/run_eval.py
@@ -9,6 +9,7 @@ from run_qa import main
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('-m', '--model', dest="model_path", required=True)
+    parser.add_argument('-o', '--output', dest="output_path")
     parser.add_argument('-b', '--batch-size', dest="batch_size", default=24)
     args = parser.parse_args()
 
@@ -21,15 +22,19 @@ if __name__ == "__main__":
         #"mrqa": [None],
     }
 
+    base_output_path = args.output_path
+    if base_output_path is None:
+        base_output_path = args.model_path
+
     for dataset, configs in datasets.items():
         for config in configs:
             gc.collect()
             torch.cuda.empty_cache()
             if config is not None:
-                output_dir = os.path.join(args.model_path, f"eval_{dataset}_{config}")
+                output_dir = os.path.join(base_output_path, f"eval_{dataset}_{config}")
                 extra_args = ["--dataset_config_name", config]
             else:
-                output_dir = os.path.join(args.model_path, f"eval_{dataset}")
+                output_dir = os.path.join(base_output_path, f"eval_{dataset}")
                 extra_args = []
             raw_args = [
                 "--model_name_or_path", args.model_path,
@@ -46,6 +51,6 @@ if __name__ == "__main__":
                 "--preprocessing_num_workers", "6",
                 "--eval_accumulation_steps", "2",
                 "--overwrite_output_dir", "False",
-                #"--overwrite_cache", "True",
+                "--overwrite_cache", "True",
             ] + extra_args
             main(raw_args=raw_args)
